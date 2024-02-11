f90f347c8c6bbc9fd89f73f959ad12debdb5fdd0
Monisha Sarkar <146477514+MonishaSarkar@users.noreply.github.com>
2024-02-11 17:14:13 +0600

    Add files via upload

---
+++ LabTask4/task1.php
@@ -0,0 +1,17 @@
+<?php
+$length = 10;
+$width = 5;
+
+// Calculate area
+$area = $length * $width;
+
+// Calculate perimeter
+$perimeter = 2 * ($length + $width);
+
+// Display the results
+echo "For a rectangle with length $length and width $width:\n";
+echo "Area: $area square units\n";
+echo "Perimeter: $perimeter units\n";
+
+
+?>
