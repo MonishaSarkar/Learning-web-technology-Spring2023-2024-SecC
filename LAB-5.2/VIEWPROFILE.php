ff59841968bdf813b1e2e9f50dd1db330e4c78bd
Monisha Sarkar <146477514+MonishaSarkar@users.noreply.github.com>
2024-02-26 22:30:22 +0600

    Update VIEWPROFILE.php

--- LAB-5.2/VIEWPROFILE.php
+++ LAB-5.2/VIEWPROFILE.php
@@ -1,10 +1,3 @@
-<?php
-
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,15 +8,16 @@
 <body>
     <table border="1" cellspacing="0" align="center" cellpadding="5">
         <tr>
-            <td><img src="img/logo.png" alt="" height="70px"></td>
-            <td width="700px" align="right">
+            <td><img src="img/logo.png" alt="" height="70px">
             <h1 align="left">XCompany</h1>
+        </td>
+            <td width="500px" align="right">
                 Logged In as <a href="login.html">Bob</a>|
                 <a href="logout.html">Logout</a>
             </td>
         </tr>
         <tr>
-            <td align="center">Account <hr>
+            <td align="left"><b>Account</b> <hr>
                 <ul>
                     <li><a href="dashboard.php">DashBoard</a></li>
                     <li><a href="viewprofile.php">View Profile</a></li>
@@ -36,38 +30,45 @@
             <td >
                 <fieldset>
                     <legend><b>PROFILE</b></legend>
+                    <img align = "right"src="https://th.bing.com/th/id/OIP.LfUcKCHKBamgN20k7KQWeAHaHT?rs=1&pid=ImgDetMain" alt="img" height="150px">
+                    
                     <table>
+                    
                         <tr>
-                            <td>Name  :Bob</td>
+                            <td>Name : </td>
                             <td></td>
                         </tr>
                         <tr>
                             <td colspan="2"><hr></td>
                         </tr>
                         <tr>
-                            <td>Email  :bob@aiub.edu </td>
+                            <td>Email  :</td>
                             <td></td>
                         </tr>
                         <tr>
                             <td colspan="2"><hr></td>
                         </tr>
                         <tr>
-                            <td>Gender  :Male</td>
+                            <td>Gender  :</td>
                             <td></td>
                         </tr>
                         <tr>
                             <td colspan="2"><hr></td>
                         </tr>
                         <tr>
-                            <td>Date of Birth   :19/09/1998</td>
+                            <td>Date of Birth   :</td>
                             <td></td>
                         </tr>
                         <tr>
                             <td colspan="2"><hr></td>
                         </tr>
                         <tr>
-                            <td colspan="2"><a href="editprofile.php">Edit Profile.</a></td>
+                            <td colspan="2"><a href="editprofile.php">Edit Profile.</a>
+                            </td>
+                            <td><a align = "right" href = "change.php"> Change</a></td>
+                            
                         </tr>
+                        
                     </table>
                 </fieldset>
             </td>
